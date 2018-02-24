a0991a8c4c4a19fd3f0e25140f79b66d94a6bb17
Vincent <VBonanni56@gmail.com>
2018-02-24 16:35:04 -0500

    Transferred Code to new project

---
+++ BillysTestConsole/AdditionComsole/AdditionComsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionComsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BillysTestConsole/addition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+
+        }
+    }
+}
